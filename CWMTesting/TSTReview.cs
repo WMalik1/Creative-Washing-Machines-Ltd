acf0c335cfe9d79c13b9c15776acbf86033e244c
p2501716 <p2501716@my365.dmu.ac.uk>
2020-02-07 11:25:08 +0000

    Added testing and class for address managment

---
+++ CWMTesting/TSTReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CWMTesting
+{
+    [TestClass]
+    public class TSTReview
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
