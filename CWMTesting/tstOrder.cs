1fa5ba56319c275fbac24fc80d8fddb8338e6a59
Tom <p2425105@my365.dmu.ac.uk>
2020-02-19 13:07:23 +0000

    Added testing and class for Order Managment

---
+++ CWMTesting/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CWMClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CWMTesting
+{
+	[TestClass]
+	public class tstOrder
+	{
+		[TestMethod]
+		public void InstanceOK()
+		{
+			clsOrder AnOrder = new clsOrder();
+			Assert.IsNotNull(AnOrder);
+		}
+	}
+}
