2edcad70dc42c75359a7be766d83cc7a5b2a137f
p2425105 <p2425105@GWHXX679WIN009W>
2020-02-20 17:08:22 +0000

    All details are now added for Order

--- CWMClasses/clsOrder.cs
+++ CWMClasses/clsOrder.cs
@@ -2,5 +2,16 @@
 {
     public class clsOrder
     {
+        public int OrderID { get; set; }
+
+        public int customerID { get; set; }
+
+        public int staffID { get; set; }
+
+        public int Product_code { get; set; }
+
+        public int Total_cost { get; set; }
+
+        public System.DateTime Date_delivery { get; set; }
     }
 }
--- CWMTesting/tstOrder.cs
+++ CWMTesting/tstOrder.cs
@@ -13,5 +13,60 @@ namespace CWMTesting
 			clsOrder AnOrder = new clsOrder();
 			Assert.IsNotNull(AnOrder);
 		}
-	}
+
+        [TestMethod]
+        public void OrderIDOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.OrderID = testData;
+            Assert.AreEqual(AnOrder.OrderID, testData);
+        }
+
+        [TestMethod]
+        public void customerIDOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder. customerID = testData;
+            Assert.AreEqual(AnOrder.customerID, testData);
+        }
+
+        [TestMethod]
+        public void staffIDOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.staffID = testData;
+            Assert.AreEqual(AnOrder.customerID, testData);
+        }
+        
+        [TestMethod]
+        public void Product_codeOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.Product_code = testData;
+            Assert.AreEqual(AnOrder.Product_code, testData);
+        }
+
+        [TestMethod]
+        public void Total_costOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            Int32 testData = 1;
+            AnOrder.Total_cost = testData;
+            Assert.AreEqual(AnOrder.Total_cost, testData);
+        }
+
+        [TestMethod]
+        public void Date_deliveryOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime testData = System.DateTime.Now.Date;
+            AnOrder.Date_delivery = testData;
+            Assert.AreEqual(AnOrder.Date_delivery, testData);
+
+        }
+    }
 }
