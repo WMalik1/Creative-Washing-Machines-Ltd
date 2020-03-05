10339b97e9c9933023d250755888e4b83c8d8fb7
Jake Effa <P2414399@my365.dmu.ac.uk>
2020-03-05 17:13:19 +0000

    Added Middle Layer

--- CWMFrontOffice/AStaff.aspx.cs
+++ CWMFrontOffice/AStaff.aspx.cs
@@ -10,10 +10,21 @@ public partial class AStaff : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStaff AStaff = new clsStaff();
+        AStaff = (clsStaff)Session["AnAddress"];
+        Response.Write(AStaff.staff_id);
 
     }
     protected void btnOK_Click(object sender, EventArgs e)
     {
-       
+        clsStaff AStaff = new clsStaff();
+        AStaff.staff_id = Convert.ToInt32(txtStaffID.Text);
+        AStaff.Name = txtName.Text;
+        AStaff.Email = txtEmail.Text;
+        AStaff.Hire_Date = Convert.ToDateTime(txtHireDate.Text);
+        AStaff.Active = Convert.ToBoolean(txtActive.Text);
+        AStaff.Salary = Convert.ToDouble(txtSalary.Text);
+        Session["AStaff"] = AStaff;
+        Response.Redirect("StaffViewer.aspx");
     }
 }
