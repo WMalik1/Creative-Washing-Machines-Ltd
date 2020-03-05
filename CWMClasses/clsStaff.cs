a58747eb9c60b4d06369ba3e1636047911cbd339
Jake Effa <P2414399@my365.dmu.ac.uk>
2020-03-05 16:47:01 +0000

    Added testing and class for staff management

--- CWMClasses/clsStaff.cs
+++ CWMClasses/clsStaff.cs
@@ -1,6 +1,112 @@
-﻿namespace CWMClasses
+﻿using System;
+
+namespace CWMClasses
 {
     public class clsStaff
     {
+        private int mstaff_id;
+        private double mSalary;
+        private string mEmail;
+        private string mName;
+        private bool mActive;
+        private DateTime mHire_Date;
+
+        public int staff_id
+        {
+            get
+            {
+                return mstaff_id;
+            }
+            set
+            {
+                mstaff_id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return mEmail;
+            }
+            set
+            {
+                mEmail = value;
+            }
+        }
+        public bool Active
+        {
+            get
+            {
+                return mActive;
+            }
+            set
+            {
+                mActive = value;
+            }
+        }
+        public double Salary
+        {
+            get
+            {
+                return mSalary;
+            }
+            set
+            {
+                mSalary = value;
+            }
+        }
+        public DateTime Hire_Date
+        {
+            get
+            {
+                return mHire_Date;
+            }
+            set
+            {
+                mHire_Date = value;
+            }
+        }
+
+        public bool Find(int staff_id)
+        {
+
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.AddParameter("@staff_id", staff_id);
+            DB.Execute("sproc_staff_FilterByStaffID");
+
+            if (DB.Count == 1)
+            {
+
+                mstaff_id = Convert.ToInt32(DB.DataTable.Rows[0]["staff_id"]);
+                mName = Convert.ToString(DB.DataTable.Rows[0]["Name"]);
+                mEmail = Convert.ToString(DB.DataTable.Rows[0]["Email"]);
+                mActive = Convert.ToBoolean(DB.DataTable.Rows[0]["Active"]);
+                mHire_Date = Convert.ToDateTime(DB.DataTable.Rows[0]["Hire_Date"]);
+                mSalary = Convert.ToDouble(DB.DataTable.Rows[0]["Salary"]);
+
+                return true;
+
+            }
+
+            else
+            {
+
+                return false;
+            }
+            
+        }
     }
 }
--- CWMTesting/tstStaff.cs
+++ CWMTesting/tstStaff.cs
@@ -13,5 +13,177 @@ namespace CWMTesting
             clsStaff AnStaff = new clsStaff();
             Assert.IsNotNull(AnStaff);
         }
+
+        [TestMethod]
+        public void Staff_idOK()
+        {
+            clsStaff staff = new clsStaff();
+            Int32 testData = 1;
+            staff.staff_id = testData;
+            Assert.AreEqual(staff.staff_id, testData);
+        }
+
+        [TestMethod]
+        public void NameOK()
+        {
+            clsStaff staff = new clsStaff();
+            string testData = "Jake Effa";
+            staff.Name = testData;
+            Assert.AreEqual(staff.Name, testData);
+        }
+
+        [TestMethod]
+        public void Email_addressOK()
+        {
+            clsStaff staff = new clsStaff();
+            string testData = "a@a.com";
+            staff.Email = testData;
+            Assert.AreEqual(staff.Email, testData);
+        }
+
+        [TestMethod]
+        public void ActiveOK()
+        {
+            clsStaff staff = new clsStaff();
+            bool testData = true;
+            staff.Active = testData;
+            Assert.AreEqual(staff.Active, testData);
+        }
+
+        [TestMethod]
+        public void SalaryOK()
+        {
+            clsStaff staff = new clsStaff();
+            double testData = int.MaxValue;
+            staff.Salary = testData;
+            Assert.AreEqual(staff.Salary, testData);
+        }
+
+
+        [TestMethod]
+        public void Hire_DateOK()
+        {
+            clsStaff staff = new clsStaff();
+            DateTime testData = DateTime.Now.Date;
+            staff.Hire_Date = testData;
+            Assert.AreEqual(staff.Hire_Date, testData);
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+            Assert.IsTrue(Found);
+        }
+
+
+        [TestMethod]
+        public void TestStaffIDFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.staff_id != 1)
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestNameFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.Name != "Jake Effa")
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestEmailFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.Email != "random.email@email.co.uk")
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
+
+        [TestMethod]
+        public void TestActiveFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.Active != false)
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestHireDateFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.Hire_Date.ToString("dd-MM-yyyy").Equals("05/03/2020"))
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
+
+        [TestMethod]
+        public void TestSalaryFound()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 staff_id = 1;
+            Found = AStaff.Find(staff_id);
+
+            if (AStaff.Salary != 1000.00)
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+
     }
 }
