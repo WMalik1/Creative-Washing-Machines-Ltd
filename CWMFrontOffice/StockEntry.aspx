e481672bcf92d2660169f6c7e412c2f927027889
Waqaar Malik <P17186239@my365.dmu.ac.uk>
2020-03-19 17:43:01 +0000

    Added collections class

--- CWMFrontOffice/StockEntry.aspx
+++ CWMFrontOffice/StockEntry.aspx
@@ -7,12 +7,12 @@
     <title></title>
     <style type="text/css">
         #form1 {
-            height: 323px;
+            height: 341px;
             width: 358px;
         }
     </style>
 </head>
-<body style="height: 325px; width: 359px">
+<body style="height: 337px; width: 359px">
     <form id="form1" runat="server">
         
         <asp:Label ID="lblProductCode" runat="server" Text="Product Code:"></asp:Label>
@@ -24,20 +24,27 @@
         <br />
         <asp:TextBox ID="txtDescription" runat="server" TextMode="MultiLine" Width="325px" Height="34px"></asp:TextBox>
         <br />
-        <asp:Label ID="lblPrice" runat="server" AssociatedControlID="txtPrice" Text="Price: "></asp:Label><asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+        <asp:Label ID="lblPrice" runat="server" AssociatedControlID="txtPrice" Text="Price: "></asp:Label>
         <br />
-        <asp:Label ID="lblNext_IntakeText" runat="server" AssociatedControlID="txtNext_Intake" Text="Next Intake: "></asp:Label><asp:TextBox ID="txtNext_Intake" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblNext_IntakeText" runat="server" AssociatedControlID="txtNext_Intake" Text="Next Intake: "></asp:Label>
+        <br />
+        <asp:TextBox ID="txtNext_Intake" runat="server"></asp:TextBox>
         <br />
         <asp:Label ID="lblPristine" runat="server" AssociatedControlID="txtPristine" Text="Pristine: "></asp:Label>
+        <br />
         <asp:TextBox ID="txtPristine" runat="server"></asp:TextBox>
         <br />
-        <asp:Label ID="lblNon_Pristine" runat="server" AssociatedControlID="txtNon_Pristine" Text="Non-Pristine?"></asp:Label>
+        <asp:Label ID="lblNon_Pristine" runat="server" AssociatedControlID="txtNon_Pristine" Text="Non-Pristine: "></asp:Label>
+        <br />
         <asp:TextBox ID="txtNon_Pristine" runat="server"></asp:TextBox>
         <br />
         <asp:CheckBox ID="checkClearence" runat="server" Text="Clearence?" />
         <br />
         <asp:Button ID="btnOK" runat="server" Text="OK" OnClick="btnOK_Click" />
         <asp:Button ID="btnCancel" runat="server" Text="Cancel" />
+        <br />
         <asp:Label ID="lblError" runat="server" ></asp:Label>
 
     </form>
---
+++ CWMTesting/tstStockCollection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using CWMClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CWMTesting
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+
+            Assert.IsNotNull(AllStock);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+
+            List<clsStock> TestList = new List<clsStock>();
+
+            clsStock TestItem = new clsStock();
+
+            TestItem.Clearence = true;
+            TestItem.Description = "example";
+            TestItem.Price = 9999.99;
+            TestItem.Pristine = 12;
+            TestItem.Non_Pristine = 15;
+            TestItem.Next_Intake = DateTime.Now.Date;
+
+            TestList.Add(TestItem);
+
+            AllStock.StockList = TestList;
+
+            Assert.AreEqual(AllStock.StockList, TestList);
+
+        }
+
+        [TestMethod]
+        public void ThisStockPropertyOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+
+            clsStock TestStock = new clsStock();
+
+            TestStock.Clearence = true;
+            TestStock.Description = "example";
+            TestStock.Price = 9999.99;
+            TestStock.Pristine = 12;
+            TestStock.Non_Pristine = 15;
+            TestStock.Next_Intake = DateTime.Now.Date;
+
+            AllStock.ThisStock = TestStock;
+
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStockCollection AllStock = new clsStockCollection();
+
+            List<clsStock> TestList = new List<clsStock>();
+
+            clsStock TestItem = new clsStock();
+
+            TestItem.Clearence = true;
+            TestItem.Description = "example";
+            TestItem.Price = 9999.99;
+            TestItem.Pristine = 12;
+            TestItem.Non_Pristine = 15;
+            TestItem.Next_Intake = DateTime.Now.Date;
+
+            TestList.Add(TestItem);
+
+            AllStock.StockList = TestList;
+
+            Assert.AreEqual(AllStock.Count, TestList.Count);
+        }
+
+    }
+}
