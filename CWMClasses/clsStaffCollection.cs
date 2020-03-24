d2f5c94ce0b872a33ab5f3751480904fc3229afc
Jake Effa <P2414399@my365.dmu.ac.uk>
2020-03-24 21:18:00 +0000

    Added Collection Class to Staff

---
+++ CWMClasses/clsStaffCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CWMClasses
+{
+    public class clsStaffCollection
+    {
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        public clsStaff ThisStaff { get; set; }
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                return mStaffList;
+            }
+            set
+            {
+                mStaffList = value;
+
+            }
+        }
+
+
+        
+        public int Count
+        {
+            get
+            {
+
+                return mStaffList.Count;
+
+            }
+            set
+
+            {
+
+
+            }
+
+        }
+
+
+        public clsStaffCollection()
+        {
+
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_staff_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsStaff AStaff = new clsStaff();
+
+                AStaff.staff_id = Convert.ToInt32(DB.DataTable.Rows[Index]["staff_id"]);
+                AStaff.Name = Convert.ToString(DB.DataTable.Rows[Index]["name"]);
+                AStaff.Email = Convert.ToString(DB.DataTable.Rows[Index]["email_address"]);
+                AStaff.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["active"]);
+                AStaff.Hire_Date = Convert.ToDateTime(DB.DataTable.Rows[Index]["hire_date"]);
+                AStaff.Salary = Convert.ToDouble(DB.DataTable.Rows[Index]["salary"]);
+
+                mStaffList.Add(AStaff);
+
+                Index++;
+            }
+
+        }
+    }
+
+
+
+
+}
---
+++ CWMTesting/tstStaffCollection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CWMClasses;
+using System.Collections.Generic;
+
+namespace CWMTesting
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            Assert.IsNotNull(AllStaff);
+
+        }
+
+        [TestMethod]
+        public void StaffPropertyOk()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff TestStaff = new clsStaff();
+
+            TestStaff.staff_id = 1;
+            TestStaff.Name = "Jake,Effa";
+            TestStaff.Email = "effaj@gmail.com";
+            TestStaff.Hire_Date = DateTime.Now.Date;
+            TestStaff.Salary = 1000.00;
+            TestStaff.Active = true;
+            TestList.Add(TestStaff);
+            AllStaff.StaffList = TestList;
+            Assert.AreEqual(AllStaff.StaffList, TestList);
+
+        }
+
+        [TestMethod]
+        public void ThisStaffPropertyOK()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            clsStaff TestStaff = new clsStaff();
+
+            TestStaff.staff_id = 1;
+            TestStaff.Name = "Jake,Effa";
+            TestStaff.Email = "effaj@gmail.com";
+            TestStaff.Hire_Date = DateTime.Now.Date;
+            TestStaff.Salary = 1000.00;
+            TestStaff.Active = true;
+
+            AllStaff.ThisStaff = TestStaff;
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+
+
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff TestStaff = new clsStaff();
+
+            TestStaff.staff_id = 1;
+            TestStaff.Name = "Jake,Effa";
+            TestStaff.Email = "effaj@gmail.com";
+            TestStaff.Hire_Date = DateTime.Now.Date;
+            TestStaff.Salary = 1000.00;
+            TestStaff.Active = true;
+
+            TestList.Add(TestStaff);
+
+            AllStaff.StaffList = TestList;
+
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
+        }
+
+        
+    }
+}
